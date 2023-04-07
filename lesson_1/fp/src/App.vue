37ce086893a3943cf71e15e04ae6bf611b87fc83
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-04-07 21:38:02 +0300

    feat: vue calculator

--- lesson_1/fp/src/App.vue
+++ lesson_1/fp/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Calculator />
 </template>
 
 <script>
+import Calculator from "./components/calc-ulator.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Calculator,
   },
 };
 </script>
---
+++ lesson_1/fp/src/components/calc-ulator.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="display">
+      <input v-model.number="operand1" type="number" />
+      <input v-model.number="operand2" type="number" />
+      =
+      {{ result }}
+    </div>
+    <div class="keyboards">
+      <button @click="sumOperands(operand1, operand2)">+</button>
+      <button @click="subtractOperands">-</button>
+      <button @click="result = operand1 * operand2">*</button>
+      <button @click="result = operand1 / operand2">/</button>
+      <button @click="inPower(operand1, operand2)">**</button>
+      <button @click="floorDiv">//</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "calc-ulator",
+  data() {
+    return {
+      operand1: 0,
+      operand2: 0,
+      result: 0,
+    };
+  },
+  methods: {
+    onChangeInput(event) {
+      console.log(event);
+      this.operand3 = event.target.value;
+    },
+    sumOperands(operand1, operand2) {
+      this.result = operand1 + operand2;
+    },
+    subtractOperands() {
+      this.result = this.operand1 - this.operand2;
+    },
+    inPower(operand1, operand2) {
+      this.result = operand1 ** operand2;
+    },
+    floorDiv() {
+      this.result = Math.floor(this.operand1 / this.operand2);
+    },
+  },
+};
+</script>
